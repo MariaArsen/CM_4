5d87747a57f49161b53c56df77d89914af1c31ef
Солнце <@LAPTOP-DPKNJGCI>
2023-01-20 21:54:10 +0300

    Добавьте файлы проекта.

---
+++ task 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int j = 0;
+            int n = 0;
+            while (j==n)
+            {
+                n += a - c;
+                j += b - c;
+            }
+            Console.WriteLine("Количество квадратов, размещенных на прямоугольнике {0}", n);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int negative, positive;
+            Console.WriteLine("Введите числа");
+            int number;
+            negative = 0; 
+            positive = 0;
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number < 0)  negative++;
+                else if (number > 0)  positive++;  
+            }
+            while (number != 0);
+            if (positive > negative)
+            {
+                Console.WriteLine("Количестов положительных цифр больше");
+            }
+            else if (positive < negative)
+            {
+                Console.WriteLine("Количество отрицательных цифр больше");
+            }
+            else if (positive == negative)
+            {
+                Console.WriteLine("Введено одинаковое количество положительных и отрицательных чисел");
+            }
+            Console.ReadKey();
+        }
+    }
+}
